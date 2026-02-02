fbed51875d856c49dcca9ae1e0a52213ff536e3e
Trustin326 <trustin326@icloud.com>
2026-02-02 14:52:38 -0500

    Create owner.js

---
+++ owner.js
@@ -0,0 +1,10 @@
+let owner=false
+
+function loginOwner(){
+owner=true
+document.getElementById("status").innerText="Owner - Unlimited Access"
+}
+
+function openDashboard(){
+alert("Owner Dashboard Open\nAffiliate Tracking Active\nAll Bots Unlocked")
+}
